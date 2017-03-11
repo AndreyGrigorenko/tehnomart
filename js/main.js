5f76fce7317f52029ece5a93809dfc2bca8414a6
AndreyGrigorenko <grigorenko584@gmail.com>
2017-03-11 22:45:55 +0200

    18

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@ $(document).ready(function(){
 	$(".map-img").click(function(){
 		$(".map").addClass("show");
 	});
-	$(".map-close").click(function(){
+	$(".form-close").click(function(){
 		$(".map").removeClass("show");
 	});
+
 
 	$(".write-to-us").click(function(){
 		$(".form").slideDown();
 	});
 	$(".form-close").click(function(){
 		$(".form").slideUp();
+	});
+
+
+	$(".buy").click(function(){
+		$(".add-product-form").addClass("show");
+	});
+
+	$(".form-close, .add-product-block-2 a:last-child").click(function(){
+		$(".add-product-form").removeClass("show");
+	
 	});
 });
